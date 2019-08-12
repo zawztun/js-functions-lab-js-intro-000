b97887d4109ba3b9cac9569195fb7de07ec947e7
Donal tun <americanarakan@gmail.com>
2019-08-12 03:56:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidaysTo(holiday, name){
+  return `Happy ${holiday}, ${name}`
+}
+
+function holidayCountdown(holiday, days){
+  return `it's ${days} days until ${holiday}!`
+}
